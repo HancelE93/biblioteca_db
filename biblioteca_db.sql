64c622c606ea5e6519e6c3301a7b5ed747b42433
HancelE1993 <hancelxyz@gmail.com>
2026-05-26 00:04:39 -0500

    Parte 4 – Insertar Datos

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -33,3 +33,27 @@ create table libro_autor (
 select * from libros;
 select * from autores;
 select * from libro_autor;
+
+-- Parte 4: Insertar Datos
+-- Insertar libros
+insert into libros (codigo, titulo, paginas) values
+('L1', 'Cien Años', 350),
+('L2', 'El Principito', 120),
+('L3', 'Don Quijote', 500),
+('L4', 'Harry Potter', 420);
+
+-- Insertar autores
+insert into autores (id, nombre, pais) values
+(1, 'Gabriel Garcia Marquez', 'Colombia'),
+(2, 'Miguel de Cervantes', 'España'),
+(3, 'J. K. Rowling', 'Inglaterra');
+
+-- Insertar relaciones libro_autor
+insert into libro_autor 
+(la_libro_codigo_fk, la_autor_id_fk, anio_publicacion)
+values
+('L1', 1, 1967),
+('L2', 1, 1943),
+('L3', 2, 1605),
+('L4', 3, 1997),
+('L1', 2, 2000);
